57b298d26a007f2d61d97ec8787616b9f7d14b08
b4shtirk1n <wowan4512@gmail.com>
2022-03-30 15:28:26 +0500

    correct config and loger

--- msglogger.py
+++ msglogger.py
@@ -8,7 +8,8 @@ client = discord.Client()
 @client.event
 async def on_message(message):
     if client.get_guild(Cserver):
-        log = print('канал - {0.channel}, Сообщение от {0.author}: {0.content}. дата: {0.created_at}'.format(message), file=open('msglog.txt', 'a'))
+        log = 'канал - {0.channel}, Сообщение от {0.author}: {0.content}. дата: {0.created_at}'.format(message)
+        print(log, file=open('msglog.txt', 'a'))
         print(log)
 
 client.run(token)
